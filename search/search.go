ac5e21c6640ec7c1f12b14c801f4f0baeff17305
Megha I <megha.i@globallogic.com>
2024-05-30 16:46:03 +0530

    Add new changes

--- search/search.go
+++ search/search.go
@@ -27,6 +27,42 @@ func ByDescription(params Params) (result []Emoji) {
 	return
 }
 
+// ByTags search for emojis with specified tags
+func ByTags(tags ...string) (result []Emoji) {
+	for _, emo := range emjis {
+		for _, tag := range tags {
+			if slices.Contains(emo.Tags, tag) {
+				result = append(result, emo)
+			}
+		}
+	}
+
+	return
+}
+
+// the specified emoji icon
+func EmojiLike(emoji string) (result []Emoji) {
+	// find the emoji
+	var found Emoji
+	for _, emo := range emjis {
+		if emo.Emoji == emoji {
+			found = emo
+			break
+		}
+	}
+
+	if found.Emoji == "" {
+		return
+	}
+
+	// find emoji with similar traits
+	var includes []string
+	includes = append(includes, found.Tags...)
+	includes = append(includes, found.Label)
+
+	return ByDescription(Params{Include: includes})
+}
+
 // shouldExclude checks if an emoji should be excluded based on the exclude criteria.
 func shouldExclude(emo Emoji, excludes []string) bool {
 	for _, exclude := range excludes {
